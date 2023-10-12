896aedfe831fcf2b21d154951f6ded05b52f788d
Aryan Mehta <aryannmehta@gmail.com>
2023-10-12 15:37:22 +0530

    ✨ Feat: Add buttons to navbar

--- app/(marketting)/_components/Navbar.tsx
+++ app/(marketting)/_components/Navbar.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+
 import { useScrollTop } from "@/hooks/use-scroll-top";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 import { Logo } from "./Logo";
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -16,6 +21,23 @@ export const Navbar = () => {
     )}>
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && (
+          <p>Loading...</p>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log In
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">
+                Get Jotion Free
+              </Button>
+            </SignInButton>
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
