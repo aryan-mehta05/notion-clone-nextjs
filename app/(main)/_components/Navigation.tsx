201e0d8d0db8933be81fb18edc8028600e464df4
Aryan Mehta <aryannmehta@gmail.com>
2023-10-12 17:55:53 +0530

    ✨ Feat: Sidebar toggle and resize

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -12,11 +12,11 @@ import {
 import { useParams, usePathname, useRouter } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
-import { useMutation } from "convex/react";
+// import { useMutation } from "convex/react";
 // import { toast } from "sonner";
 
 import { cn } from "@/lib/utils";
-import { api } from "@/convex/_generated/api";
+// import { api } from "@/convex/_generated/api";
 // import {
 //   Popover,
 //   PopoverTrigger,
@@ -25,17 +25,17 @@ import { api } from "@/convex/_generated/api";
 // import { useSearch } from "@/hooks/use-search";
 // import { useSettings } from "@/hooks/use-settings";
 
-// import { UserItem } from "./user-item";
 // import { Item } from "./item";
-// import { DocumentList } from "./document-list";
-// import { TrashBox } from "./trash-box";
 // import { Navbar } from "./navbar";
+// import { TrashBox } from "./trash-box";
+// import { UserItem } from "./user-item";
+// import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
-  const router = useRouter();
+  // const router = useRouter();
   // const settings = useSettings();
   // const search = useSearch();
-  const params = useParams();
+  // const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   // const create = useMutation(api.documents.create);
@@ -46,19 +46,19 @@ export const Navigation = () => {
   const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
-  // useEffect(() => {
-  //   if (isMobile) {
-  //     collapse();
-  //   } else {
-  //     resetWidth();
-  //   }
-  // }, [isMobile]);
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    } else {
+      resetWidth();
+    }
+  }, [isMobile]);
 
-  // useEffect(() => {
-  //   if (isMobile) {
-  //     collapse();
-  //   }
-  // }, [pathname, isMobile]);
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    }
+  }, [pathname, isMobile]);
 
   const handleMouseDown = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -91,23 +91,23 @@ export const Navigation = () => {
     document.removeEventListener("mouseup", handleMouseUp);
   };
 
-  // const resetWidth = () => {
-  //   if (sidebarRef.current && navbarRef.current) {
-  //     setIsCollapsed(false);
-  //     setIsResetting(true);
+  const resetWidth = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(false);
+      setIsResetting(true);
 
-  //     sidebarRef.current.style.width = isMobile ? "100%" : "240px";
-  //     navbarRef.current.style.setProperty(
-  //       "width",
-  //       isMobile ? "0" : "calc(100% - 240px)"
-  //     );
-  //     navbarRef.current.style.setProperty(
-  //       "left",
-  //       isMobile ? "100%" : "240px"
-  //     );
-  //     setTimeout(() => setIsResetting(false), 300);
-  //   }
-  // };
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+      navbarRef.current.style.setProperty(
+        "width",
+        isMobile ? "0" : "calc(100% - 240px)"
+      );
+      navbarRef.current.style.setProperty(
+        "left",
+        isMobile ? "100%" : "240px"
+      );
+      setTimeout(() => setIsResetting(false), 300);
+    }
+  };
 
   const collapse = () => {
     if (sidebarRef.current && navbarRef.current) {
@@ -119,7 +119,7 @@ export const Navigation = () => {
       navbarRef.current.style.setProperty("left", "0");
       setTimeout(() => setIsResetting(false), 300);
     }
-  }
+  };
 
   // const handleCreate = () => {
   //   const promise = create({ title: "Untitled" })
@@ -192,7 +192,7 @@ export const Navigation = () => {
         </div>
         <div
           onMouseDown={handleMouseDown}
-          // onClick={resetWidth}
+          onClick={resetWidth}
           className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
         />
       </aside>
@@ -216,7 +216,7 @@ export const Navigation = () => {
         )} */}
         <nav className="bg-transparent px-3 py-2 w-full">
           {isCollapsed && <MenuIcon 
-            // onClick={resetWidth} 
+            onClick={resetWidth} 
             role="button" 
             className="h-6 w-6 text-muted-foreground" 
           />}
