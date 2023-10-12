486d2bf09651d6244fe0d11b5107a6fba0803292
Aryan Mehta <aryannmehta@gmail.com>
2023-10-12 14:25:15 +0530

    ✨ Feat: Finish navbar, add dark/light mode

--- app/(marketting)/_components/Navbar.tsx
+++ app/(marketting)/_components/Navbar.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { ModeToggle } from "@/components/mode-toggle";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
+
 import { Logo } from "./Logo";
 
 export const Navbar = () => {
@@ -9,10 +11,13 @@ export const Navbar = () => {
 
   return (
     <div className={cn(
-      "z-50 bg-background fixed top-0 flex items-center w-full p-6", 
+      "z-50 bg-background dark:bg-[#1f1f1f] fixed top-0 flex items-center w-full p-6", 
       scrolled && "border-b shadow-sm"
     )}>
       <Logo />
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        <ModeToggle />
+      </div>
     </div>
   );
 };
