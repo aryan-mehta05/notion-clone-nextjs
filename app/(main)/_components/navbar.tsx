98777df604b1161de7777200020f879544323050
Aryan Mehta <aryannmehta@gmail.com>
2023-10-14 15:14:05 +0530

    ✨ Feat: Create navbar for main screen

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -18,5 +18,47 @@ export const Navbar = ({
 }: NavbarProps) => {
   const params = useParams();
 
-  const document = useQuery(api.documents.getbyId);
-}
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId as Id<"documents">,
+  });
+
+  if (document === undefined) {
+    return (
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
+        Title
+        <div className="flex items-center gap-x-2">
+          Menu
+        </div>
+      </nav>
+    );
+  }
+
+  if (document === null) {
+    return null;
+  }
+  
+  return (
+    <>
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
+        )}
+        <div className="flex items-center justify-between w-full">
+          Title
+          <div className="flex items-center gap-x-2">
+            Publish & Menu
+          </div>
+        </div>
+      </nav>
+      {document.isArchived && (
+        <div>
+          Banner
+        </div>
+      )}
+    </>
+  );
+};
