5adf4bc1e70898cbba6fa8b93e0e87a6ab8d9ef1
Aryan Mehta <aryannmehta@gmail.com>
2023-10-16 11:10:00 +0530

    ✨ Feat: Add [documentId] page.tsx structure code

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,4 +1,49 @@
-const DocumentIdPage = () => {
+"use client";
+
+import { useMemo } from "react";
+import dynamic from "next/dynamic";
+import { useMutation, useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+};
+
+const DocumentIdPage = ({
+  params
+}: DocumentIdPageProps) => {
+  // const Editor = useMemo(() => dynamic(() => import("@/components/editor"), { ssr: false }), []);
+  
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content
+    });
+  };
+
+  if (document === undefined) {
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Not found</div>
+  }
+
   return ( 
     <div>
       Document ID
