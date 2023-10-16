39ce7213c02949eea58ae1745179dcfcfd9e579f
Aryan Mehta <aryannmehta@gmail.com>
2023-10-16 11:51:09 +0530

    ✨ Feat: Add removeIcon convex function (documents.ts)

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -5,3 +5,27 @@ import { ImageIcon, Smile, X } from "lucide-react";
 import { ElementRef, useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
+import { Doc } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { IconPicker } from "@/components/icon-picker";
+import { useCoverImage } from "@/hooks/use-cover-image";
+
+interface ToolbarProps {
+  initialData: Doc<"documents">;
+  preview?: boolean;
+};
+
+export const Toolbar = ({
+  initialData,
+  preview
+}: ToolbarProps) => {
+  const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(initialData.title);
+
+  const update = useMutation(api.documents.update);
+  const removeIcon = useMutation(api.documents.removeIcon);
+
+  const coverImage = useCoverImage();
+};
